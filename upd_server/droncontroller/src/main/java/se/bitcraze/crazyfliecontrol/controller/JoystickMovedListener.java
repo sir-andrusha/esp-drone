590b7b084f2e837384f15dbca04df508a69a2f7d
nichd147 <diniig147@gmail.com>
2025-01-22 00:48:16 +0400

    separates ground control from esp drone

--- upd_server/droncontroller/src/main/java/se/bitcraze/crazyfliecontrol/controller/JoystickMovedListener.java
+++
@@ -1,7 +0,0 @@
-package se.bitcraze.crazyfliecontrol.controller;
-
-public interface JoystickMovedListener {
-    public void OnMoved(float pan, float tilt);
-    public void OnReleased();
-    public void OnReturnedToCenter();
-}
--- upd_server/ping_pong/go.mod
+++
@@ -1,8 +0,0 @@
-module example.com/udp_server
-
-go 1.23.4
-
-require (
-	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-)
